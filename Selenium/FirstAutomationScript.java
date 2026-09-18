3f63471e20631a9d545b97c7466f96b0ef34c474
Sushma <sushma.vijay@girmiti.com>
2026-09-18 14:33:07 +0530

    ss

---
+++ Selenium/FirstAutomationScript.java
@@ -0,0 +1,8 @@
+package Selenium;
+    public class FirstAutomationScript {
+        public static void main(String[] args) {
+            System.out.println("Launching Browser...");
+            System.out.println("Navigating to target web application...");
+            System.out.println("Automation Test Passed Successfully!");
+        }
+    }
